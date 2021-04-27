64a0c9b47c16ef8484aca70ba36b62b1a5e51d42
suriya-jambunathan <82708576+suriya-jambunathan@users.noreply.github.com>
2021-04-27 14:48:19 +0400

    Add files via upload

---
+++ WebApp1.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 17 09:42:41 2021
+
+@author: suriyaprakashjambunathan
+"""
+
+#Description
+import pandas as pd
+from PIL import Image
+import streamlit as st
+import numpy as np
+from itertools import compress
+import sys
+sys.path.append('src/')
+
+from classes import *
+from constants import Wm, W0m, dm, tm, Rows, Xa, Ya
+
+import pyutilib.subprocess.GlobalData
+
+import ptvsd
+#ptvsd.enable_attach(address=('localhost', 8501))
+
+#Title and sub-title
+image = Image.open('Content/fyp_cst_ss.png')
+st.set_page_config(page_title='M.L.A.N.T.', page_icon = image, layout = 'wide', initial_sidebar_state = 'auto')
+st.write("""
+# M.L.A.N.T.
+""")
+
+#Open and display image on webapp
+
+st.image(image, caption = 'GA Class_reg', use_column_width = True)
+
+#df = pd.read_csv('Content/antenna.csv')
+
+#st.subheader('Data Information: ')
+
+#Show the data as a table
+#st.dataframe(df)
+
+#Show statistics on data
+#st.write(df.describe())
+
+#Show the data as a chart
+#chart = st.bar_chart(df)
+
+
+
+best = [2141.3747, 650.78, 77.0894892,214.13747, 7, 1200, 2141.3747]
+
+#Get Feature inout from users
+
+def get_user_input():
+    wm = st.number_input('Wm (in µm)', min_value = Wm[0], max_value = Wm[-1], value = best[0], step = (Wm[1] - Wm[0]))
+    w0m  = st.number_input('W0m (in µm)', min_value = W0m[0], max_value = W0m[-1], value = best[1], step = (W0m[1] - W0m[0]))
+    d    = st.number_input('dm (in µm)', min_value = dm[0], max_value = dm[-1], value = best[2], step = (dm[1] - dm[0]))
+    t    = st.number_input('tm (in µm)', min_value = tm[0], max_value = tm[-1], value = best[3], step = (tm[1] - tm[0]))
+    rows = st.number_input('Rows', min_value = Rows[0], max_value = Rows[-1], value = best[4], step = (Rows[1] - Rows[0]))
+    xa   = st.number_input('Xa (in µm)', min_value = Xa[0], max_value = Xa[-1], value = best[5], step = (Xa[1] - Xa[0]))
+    ya   = st.number_input('Ya (in µm)', min_value = Ya[0], max_value = Ya[-1], value = best[6], step = (Ya[1] - Ya[0]))
+    
+    #store a dictionary into a variable
+    user_data = {'Wm'   : wm,
+                 'W0m'  : w0m,
+                 'dm'   : dm,
+                 'tm'   : tm,
+                 'Rows' : rows,
+                 'Xa'   : xa,
+                 'Ya'   : ya}
+    
+    #transform the data into a dataframe
+    #features = user_data.values
+    #features = list(features)
+    return([wm, w0m, d, t, rows, xa, ya])
+    
+#Store the user inputs
+user_input = get_user_input()
+
+#Set a subheader and display user input
+#st.subheader('User Input: ')
+#st.write(user_input)
+
+'''
+'''
+
+feat_inds = [[True, False, True, True, True, True, True],
+             [True, False, True, False, True, True, True],
+             [True, False, True, True, True, True, True]]
+
+
+    
+    
+import joblib
+
+model = joblib.load('Content/trained_class_reg.sav')
+model = model[:3]
+
+#Store model predictions in a variable
+prediction = []
+for i in range(3):
+    prediction.append((model[i]).predict(list(compress(user_input, feat_inds[i] ))))
+
+
+#Set a subheader and display prediction
+st.subheader('Prediction: ')
+st.write("Bandwidth: " + str(prediction[0]))
+st.write("Gain: " + str(prediction[1]))
+st.write("VSWR: " + str(prediction[2]))
+
+
+
+#"/Users/suriyaprakashjambunathan/WebApp.py"
+    
+
+
+import sys
+from streamlit import cli as stcli
+
+if __name__ == '__main__':
+    sys.argv = ["streamlit", "run", "WebApp1.py"]
+    sys.exit(stcli.main())
+    
+    
+    
